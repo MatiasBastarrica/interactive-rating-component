4501274f25d84058c5f533524aad28b2fe72c025
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-29 15:41:45 -0300

    Display the selected rating when the modal appears after the form is submitted

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const dialog = document.querySelector("dialog");
 const form = document.querySelector("form");
+const radioInputs = document.querySelectorAll("input[type='radio']");
+const ratingSelection = dialog.querySelector(".rating-selection");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  let ratingSelected;
+  radioInputs.forEach((radioInput) => {
+    if (radioInput.checked) {
+      ratingSelected = radioInput.value;
+    }
+  });
   dialog.showModal();
+  ratingSelection.textContent = `${ratingSelected}`;
 });
